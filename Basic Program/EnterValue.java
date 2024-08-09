d1732f5e7d730653eaa9437fc5b9ecb3a28cdea8
RautMaya <suryawanshimaya28@gmail.com>
2024-08-09 22:30:01 +0530

    Practice

---
+++ Basic Program/EnterValue.java
@@ -0,0 +1,18 @@
+class Value
+{
+int num;
+
+public  void main(String[] args)
+{
+ System.out.println("num ");
+}
+}
+class Hello
+{ 
+public static void main(String[] args)
+{
+System.out.println("The Value of num :" );
+}
+}
+
+
