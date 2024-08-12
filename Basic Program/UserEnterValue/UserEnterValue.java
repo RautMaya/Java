2609fccf54a7dc967b9a3c705f83973004fa7f31
RautMaya <suryawanshimaya28@gmail.com>
2024-08-12 15:42:46 +0530

    User Enter Some and Then Print

---
+++ Basic Program/UserEnterValue/UserEnterValue.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+class UserEnterValue
+{
+int num;
+public static void main(String[] args)
+{
+Scanner scan =new Scanner(System.in);
+System.out.println("Enter Some Number");
+int num=scan.nextInt();
+
+System.out.println("You Enter Number is :");
+System.out.print(num);
+}
+}
