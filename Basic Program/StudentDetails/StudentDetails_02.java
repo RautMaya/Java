d1ffc840c63766a928c4d333cbd26af498e19f2f
RautMaya <suryawanshimaya28@gmail.com>
2024-08-19 15:55:31 +0530

    Practice Program

---
+++ Basic Program/StudentDetails/StudentDetails_02.java
@@ -0,0 +1,12 @@
+class StudentDetails02
+{
+int Id;
+String Name;
+
+ public static void main(String[] args)
+{
+StudentDetails02 Obj=new StudentDetails02 ();
+System.out.println(Obj.Id );
+System.out.println(Obj.Name);
+}
+}
